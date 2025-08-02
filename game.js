af672c5d7f6825da391b502a411a85cca1a7e2ee
mayadinq8 <sultansaidhamed@gmail.com>
2025-08-02 11:18:54 +0300

    Update game.js

--- game.js
+++ game.js
@@ -73,14 +73,13 @@ function initFirebase() {
     try {
         // قم بلصق إعدادات مشروع Firebase هنا
         const firebaseConfig = {
-  apiKey: "AIzaSyBLyoIoXp2aJCnhFqIFufMVBz0fzCS-FYY",
-  authDomain: "game-303eb.firebaseapp.com",
-  databaseURL: "https://game-303eb-default-rtdb.firebaseio.com",
-  projectId: "game-303eb",
-  storageBucket: "game-303eb.firebasestorage.app",
-  messagingSenderId: "22261863844",
-  appId: "1:22261863844:web:66f8541079b9025ec69d31",
-  measurementId: "G-RKN2F3HVHS"
+            apiKey: "AIzaSyBLyoIoXp2aJCnhFqIFufMVBz0fzCS-FYY",
+            authDomain: "game-303eb.firebaseapp.com",
+            projectId: "game-303eb",
+            storageBucket: "game-303eb.firebasestorage.app",
+            messagingSenderId: "22261863844",
+            appId: "1:22261863844:web:66f8541079b9025ec69d31",
+            measurementId: "G-RKN2F3HVHS"
         };
         
         firebaseApp = firebase.initializeApp(firebaseConfig);
